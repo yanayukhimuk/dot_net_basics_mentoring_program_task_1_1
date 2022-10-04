b5f2a0bff4310d7f1f4085c3a043bd1b2b90e33b
Yana Yukhimuk <61915492+yanayukhimuk@users.noreply.github.com>
2022-10-04 17:59:54 +0300

    Add project files.

---
+++ dot_net_basics_mentoring_program_task_1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dot_net_basics_mentoring_program_task_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+                Console.WriteLine("Hello, " + arg + "!"); 
+        }
+    }
+}
